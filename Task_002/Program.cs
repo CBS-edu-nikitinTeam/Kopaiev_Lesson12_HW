d4aeddaf9316a5c5a5d293a88c64eb1a1c98cf62
htcak <htcakuma@gmail.com>
2020-12-26 11:47:48 +0200

    Add Realization

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -6,7 +6,23 @@ namespace Task_002
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hi, type first side");
+            if (!double.TryParse(Console.ReadLine(), out double firstSide))
+            {
+                Console.WriteLine("Wrong input");
+                return;
+            }
 
+            Console.WriteLine("Hi, type second side");
+            if (!double.TryParse(Console.ReadLine(), out double secondSide))
+            {
+                Console.WriteLine("Wrong input");
+                return;
+            }
+
+            Rectangle rectangle = new Rectangle(firstSide, secondSide);
+            Console.WriteLine($"Area is {rectangle.Area}");
+            Console.WriteLine($"Perimeter is {rectangle.Perimeter}");
         }
     }
 }
--- Task_002/Rectangle.cs
+++ Task_002/Rectangle.cs
@@ -1,4 +1,42 @@
 ﻿namespace Task_002
 {
+    class Rectangle
+    {
+        double side1, side2;
 
+        // Construct
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+
+        double PerimeterCalculator()
+        {
+            return (side1 + side2) * 2;
+        }
+
+        public double Area
+        {
+            get
+            {
+                return AreaCalculator();
+            }
+
+        }
+
+        public double Perimeter
+        {
+            get
+            {
+                return PerimeterCalculator();
+            }
+        }
+
+    }
 }
